9931ac52bfeba73a5abda5e580703e5313a45b45
Charity-Biwot <biwotcharityjerop@gmail.com>
2022-03-11 12:20:45 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,32 @@
+fun main() {
+//function that prints out name and greeting
+    var name = "charity"
+    var greeting = "hello"
+    println(greeting + " " + name)
+    division(5, 3)
+    var x=addition(5, 12, 4, 6)
+    statement()
+}
+
+fun division(a:Int,b:Int){
+    var modulus = println(a % b)
+    return modulus
+}
+
+fun addition(a:Int,b:Int,c:Int,d:Int):Int {
+    var sum = a + b + c + d
+    return sum
+}
+
+fun statement() {
+    var y = "i like watching"
+    println(y)
+}
+
+
+
+
+
+
+
+
